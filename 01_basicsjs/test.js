1d7e86d207f8feee023e0bf49a08a918424e0836
Shriyansh Ozarkar <167804906+Shriyansh900@users.noreply.github.com>
2024-11-28 17:51:30 +0000

    day two of learning js

--- 01_basicsjs/test.js
+++ 01_basicsjs/test.js
@@ -1,1 +1,18 @@
-console.log("shriyansh");
+// console.log("shriyansh");
+// const accountId = 123123
+// let accountEmail = "rohit.sapkade2003@gmail.com"
+// var accountPassword = "654321"
+// accountCity = "Ratlam"
+
+// //accountId = 2 //
+
+// accountEmail = "sozarkar7@gmail.com"
+// accountPassword = "123456"
+// accountCity = "Indore"
+
+// console.table([accountId , accountEmail , accountPassword , accountCity])
+
+
+// alert(3+3) // we are using node js not browser
+
+console.log(9 + 9)
